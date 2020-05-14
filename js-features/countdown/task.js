d5955bd346ff482d62769c8473c3e40be9b50af1
makusheva <makushevaa1@gmail.com>
2020-05-14 14:05:53 +0300

    HW 1.1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,26 @@
+// Не верное решение:
+// let counter = 5;
+// function timer() {
+//     document.getElementById("timer").textContent = counter;
+//     counter--;
+//     if (counter < -1) {
+//         alert('Вы победили в конкурсе!')
+//         clearInterval(id);
+//     }
+// }
+// let id = setInterval(timer, 1000)
+// timer()
+
+// Верное решение
 let counter = 5;
 function timer() {
-    document.getElementById("timer").textContent = counter;
-    counter--;
-    if (counter < -1) {
+    if (counter < 0) {
         alert('Вы победили в конкурсе!')
         clearInterval(id);
-    }
-}
+    } else {
+		document.getElementById("timer").textContent = counter;
+		counter--;
+	}
+}   
 let id = setInterval(timer, 1000)
 timer()
